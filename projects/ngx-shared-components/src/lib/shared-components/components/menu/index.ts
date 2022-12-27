96608f06f5d286439364d059fa6e3f3a468c90e1
Rei Armenia <5091445+rarmenia@users.noreply.github.com>
2022-12-27 14:21:06 -0500

    clear

--- projects/ngx-shared-components/src/lib/shared-components/components/menu/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './common-menu.component';
-export * from './menu-button.component';
-export * from './menu-dropdown.component';
-export * from './menu-item.component';
-export * from './menu.component';
